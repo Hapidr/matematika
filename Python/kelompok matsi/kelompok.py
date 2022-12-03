2f83a93197fe9ae19d4e2148e1ba5880a835e419
Hapidr <hapidr24@gmail.com>
2022-12-03 19:57:21 +0700

    cannot replace value in cell

---
+++ Python/kelompok matsi/kelompok.py
@@ -0,0 +1,16 @@
+
+def h(k,m):
+  return k%m
+
+ruang = []
+freeSpace = int(input("Masukkan ruang memori yang tersedia : "))
+data = int(input("Jumlah data yg diinputkan : "))
+
+for i in range(freeSpace):
+  ruang.append([])
+  
+for j in range(data):
+  kunci = int(input("Masukkan kunci : "))
+  ruang[h(kunci, freeSpace)].append(kunci)
+
+print(ruang)
