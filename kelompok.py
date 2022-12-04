3669c6483f0181abfb35c574e747a82904ca0854
Hapidr <hapidr24@gmail.com>
2022-12-04 15:29:29 +0700

    add condition for move to next cell

--- kelompok.py
+++ kelompok.py
@@ -1,16 +1,23 @@
 
-def h(k,m):
-  return k%m
+def h(k, m):
+    return k % m
+
 
 ruang = []
 freeSpace = int(input("Masukkan ruang memori yang tersedia : "))
 data = int(input("Jumlah data yg diinputkan : "))
 
 for i in range(freeSpace):
-  ruang.append([])
-  if len(ruang) == freeSpace:
-    for j in range(data):
-      kunci = int(input("Masukkan kunci : "))
-      ruang[h(kunci, freeSpace)].append(kunci)
+    ruang.append([])
+    if len(ruang) == freeSpace:
+        for j in range(data):
+            kunci = int(input("Masukkan kunci : "))
 
-print(ruang)
+            if len(ruang[h(kunci, freeSpace)]) != 0:
+                ruang[h(kunci, freeSpace) + 1].append(kunci)
+                print(f"len = {len(ruang[i])}")
+            else:
+                ruang[h(kunci, freeSpace)].append(kunci)
+
+print(f"\nhasil akhir\n{ruang}")
+print(len(ruang[1]))
